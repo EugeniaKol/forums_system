dc213c1b293bb804c9063d816741fe564f07bf04
EugeniaKol <jennichka10155@gmail.com>
2020-12-12 23:51:00 +0200

    fixed example

--- client/examples.js
+++ client/examples.js
@@ -1,7 +1,7 @@
 // This file contains examples of scenarios implementation using
 // the SDK for channels management.
 
-const channels = require('./channels/client');
+const channels = require('./forums/client');
 let myUrl = 'http://localhost:8080';
 
 const client = channels.Client(myUrl);
@@ -18,10 +18,8 @@ const printForums = res => {
 
 // Scenario 1: Show existing forums.
 client.showForums()
-    .then((list) => printForums(list);
-    )
-    .catch((e) => console.log(e);
-    );
+    .then((list) => printForums(list))
+    .catch((e) => console.log(e));
 
 // Scenario 2: Create new user.
 let newNick = 'zhenshen';
@@ -32,8 +30,8 @@ client.addUser(newNick, newInters)
         console.log('Adding user:', );
         console.log('Adding channel response:', resp);
 
-    //forums now have modified 'users' field
+        //forums now have modified 'users' field
         return client.showForums()
+    })
             .then((list) => printForums(list))
-    .catch((e) => console.log(e);
-    );
+    .catch((e) => console.log(e));
