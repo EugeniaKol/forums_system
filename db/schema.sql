9933550ea03453bae7c87e0fe28fbbf249fc4136
Polina <schachowa.paulina@gmail.com>
2020-12-14 12:24:13 +0200

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,42 @@
+CREATE SCHEMA IF NOT EXISTS `forums` DEFAULT CHARACTER SET utf8 ;
+
+-- Schema forums
+USE `forums` ;
+
 -- Create tables.
-DROP TABLE IF EXISTS "channels";
-CREATE TABLE "channels"
+DROP TABLE IF EXISTS `channels`;
+CREATE TABLE `channels`
 (
-    "id"   SERIAL PRIMARY KEY,
-    "name" VARCHAR(50) NOT NULL UNIQUE
+    `id`   INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(50) NOT NULL UNIQUE,
+    `topicKeyword` VARCHAR(50) NOT NULL
+);
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`
+(
+    `id`   INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `nickname` VARCHAR(50) NOT NULL UNIQUE
+);
+
+DROP TABLE IF EXISTS `interests`;
+CREATE TABLE `interests`
+(
+    `id`   INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `users_id` INT NOT NULL,
+    `channels_id` INT NOT NULL,
+    INDEX `fk_interests_users_idx` (`users_id` ASC) VISIBLE,
+	INDEX `fk_interests_channels_idx` (`channels_id` ASC) VISIBLE,
+	CONSTRAINT `fk_interests_users`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `users` (`id`),
+	CONSTRAINT `fk_interests_channels`
+    FOREIGN KEY (`channels_id`)
+    REFERENCES `channels` (`id`)
+
 );
 
 -- Insert demo data.
-INSERT INTO "channels" (name) VALUES ('lectures');
-INSERT INTO "channels" (name) VALUES ('practice');
+INSERT INTO `channels` (`name`, `topicKeyword`) VALUES ('Політика в Україні', 'ukraine-politics');
+INSERT INTO `users` (`nickname`) VALUES ('user1');
