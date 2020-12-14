c5a575cc5b81b5066705103ee1702f62f09cf3b6
Polina <schachowa.paulina@gmail.com>
2020-12-15 00:25:52 +0200

    Update db_test.go

--- server/db/db_test.go
+++ server/db/db_test.go
@@ -4,13 +4,13 @@ import "testing"
 
 func TestDbConnection_ConnectionURL(t *testing.T) {
 	conn := &Connection{
-		DbName:     "db1",
-		User:       "user1",
-		Password:   "pass1",
+		DbName:     "forums_sys",
+		User:       "root",
+		Password:   "password",
 		Host:       "localhost",
 		DisableSSL: true,
 	}
-	if conn.ConnectionURL() != "postgres://user1:pass1@localhost/db1?sslmode=disable" {
+	if conn.ConnectionURL() != "mysql://root:password@localhost/forums_sys?sslmode=disable" {
 		t.Error("Unexpected connection string")
 	}
 }
