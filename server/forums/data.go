a8fb74df5bd1d895d71c0b6d84352eb351479569
Polina <schachowa.paulina@gmail.com>
2020-12-15 00:25:59 +0200

    Update data.go

--- server/forums/data.go
+++ server/forums/data.go
@@ -12,6 +12,13 @@ type User struct {
 	Interests []string `json:"interests"`
 }
 
+type Forum struct {
+	ID        		int64    `json:"id"`
+	Name  			string   `json:"name"`
+	TopicKeyword  	string   `json:"topicKeyword"`
+	Users 			[]string `json:"users"`
+}
+
 // Store points to db
 type Store struct {
 	Db *sql.DB
@@ -23,27 +30,65 @@ func NewStore(db *sql.DB) *Store {
 }
 
 // ListForums recieves forums from db and lists them
-func (s *Store) ListForums() ([]*User, error) {
-	rows, err := s.Db.Query("SELECT id, nickname, interests FROM channels LIMIT 200")
+func (s *Store) ListForums() ([]*Forum, error) {
+	//rows, err := s.Db.Query("SELECT id, nickname, interests FROM channels LIMIT 200")
+		rows, err := s.Db.Query("SELECT f.id, f.name, f.topicKeyword FROM forums_sys.forums f")
+
 	if err != nil {
 		return nil, err
 	}
 
 	defer rows.Close()
 
-	var res []*User
+	var res []*Forum
 	for rows.Next() {
-		var c User
-		if err := rows.Scan(&c.ID, &c.Nickname); err != nil {
+		var f Forum
+//		var users []string
+		if err := rows.Scan(&f.ID, &f.Name, &f.TopicKeyword); err != nil {
 			return nil, err
 		}
-		res = append(res, &c)
+		res = append(res, &f)
 	}
+
+	//for _, forum := range res {
+	//	id := forum.ID
+
+/*		id := 1;
+		
+		usres, _ := s.Db.Query("SELECT u.nickname FROM forums_sys.interests i, forums_sys.users u WHERE i.forums_id=$1", id)
+		if err != nil {
+		return nil, err
+	}
+
+	defer usres.Close()
+
+		var Nickname string
+		var users []string
+    	for usres.Next() {
+
+        if err := usres.Scan(&Nickname); err != nil {
+          return nil, err
+        }
+        
+        users = append(users, Nickname)
+        res[1].Users = users
+ 
+    }
+    
+*/
+
 	if res == nil {
-		res = make([]*User, 0)
+		res = make([]*Forum, 0)
 	}
-	return res, nil
+	return res, nil 
 }
+
+
+
+
+   
+
+
 
 // CreateUser creates user 
 func (s *Store) CreateUser(Nickname string, Interests []string) error {
@@ -53,6 +98,15 @@ func (s *Store) CreateUser(Nickname string, Interests []string) error {
 	if len(Interests) == 0 {
 		return fmt.Errorf("User interests is not provided")
 	}
+	/*
 	_, err := s.Db.Exec("INSERT INTO channels (nickname, interests) VALUES ($1)", Nickname, Interests)
+	return err
+	*/
+	_, err := s.Db.Exec("INSERT INTO users (nickname) VALUES ($1)", Nickname)
+	/*for i := 0; i< len(Interests); i++{
+		_, err := s.Db.Exec("insert into forums_sys.interests (`users_id`, `forums_id`) values (
+							(select u.id from forums_sys.users u where u.nickname = $1),
+							(select c.id from forums_sys.forums c where c.topicKeyword = $2))", Nickname, i)
+		}*/
 	return err
 }
